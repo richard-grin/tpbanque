f301d502978a9dd30da54c8a203eda9c58578a10
Richard Grin <grin@unice.fr>
2021-11-01 17:40:27 +0100

    Ajout de la page qui affiche les opérations bancaires et divers modifications qui y sont liées dans l'entité OperationBancaire et dans la page qui affiche la liste de tous les comptes.

--- src/main/java/fr/grin/tpbanque/entities/OperationBancaire.java
+++ src/main/java/fr/grin/tpbanque/entities/OperationBancaire.java
@@ -35,8 +35,16 @@ public class OperationBancaire implements Serializable {
     return id;
   }
 
-  public void setId(Long id) {
-    this.id = id;
+  public String getDescription() {
+    return description;
+  }
+
+  public LocalDateTime getDateOperation() {
+    return dateOperation;
+  }
+
+  public int getMontant() {
+    return montant;
   }
 
   @Override
